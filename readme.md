092d32db57a8c2765fe46b1c65334ef5a5e74b25
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-02-02 14:38:34 +0100

    removed unused files, added modelDescirpiton.xml and updated scripts to build complete fmu

---
+++ readme.md
@@ -0,0 +1,13 @@
+
+# Packages
+zip
+
+```bash
+sudo apt-get install libpthread-stubs0-dev
+```
+
+# Compilers
+
+```bash
+sudo apt-get install mingw-w64 g++-mingw-w64
+```
--- xcompile.sh
+++ xcompile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 function compileDarwin
 {
@@ -6,10 +7,10 @@ function compileDarwin
 		echo Building Darwin .dylib
 		B=$1/build/darwin
 
-		rm -r $B
+		rm -rf $B
 		mkdir -p $B
 
-		cmake  -B$B -Hthreewatertank1/ -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/osx-gcc.cmake`
+		cmake  -B$B -H$1 -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/osx-gcc.cmake`
 
 		make -C $B
 
@@ -21,10 +22,10 @@ function compileWin32
 		echo Building Win32 .dll
 		B=$1/build/win32
 
-		rm -r $B
+		rm -rf $B
 		mkdir -p $B
 
-		cmake  -B$B -Hthreewatertank1/ -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw32.cmake`
+		cmake  -B$B -H$1 -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw32.cmake`
 
 		make -C $B
 
@@ -36,10 +37,10 @@ function compileWin64
 		echo Building Win64 .dll
 		B=$1/build/win64
 
-		rm -r $B
+		rm -rf $B
 		mkdir -p $B
 
-		cmake  -B$B -Hthreewatertank1/ -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw64.cmake`
+		cmake  -B$B -H$1 -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw64.cmake`
 
 		make -C $B
 
@@ -49,13 +50,12 @@ function compileLinux64
 {
 		echo Building Linux x64 .so
 		B=$1/build/linux64
-
-		rm -r $B
+		rm -rf $B
 		mkdir -p $B
 
-		cmake  -B$B
+		cmake  -B$B -H$1
 
-		make -C $B		
+		make -C $B
 }
 
 function compileLinux32
@@ -63,23 +63,56 @@ function compileLinux32
 		echo Building Linux x32 .so
 		B=$1/build/linux32
 
-		rm -r $B
+		rm -rf $B
 		mkdir -p $B
 
-		CFLAGS=-m32 cmake  -B$B
+		cmake -B$B -H$1 -DLINK_FLAGS="-m32" -DCFLAGS="-m32"
 
 		make -C $B		
+}
+
+function assemble
+{
+
+B=$1/build/
+
+mkdir -p $B/fmu/{binaries,resources,sources}
+
+mkdir -p $B/fmu/binaries/{darwin64,win32,win64,linux32,linux64}
+
+echo Copying files...
+cp $1/modelDescription.xml $B/fmu/
+
+cp $1/sources/*.* $B/fmu/sources/
+
+BIN=$B/fmu/binaries
+cp $B/darwin/*.dylib $BIN/darwin64/
+cp $B/linux64/*.so $BIN/linux64/
+cp $B/linux32/*.so $BIN/linux32/
+cp $B/win64/*.dll $BIN/win64/
+cp $B/win32/*.dll $BIN/win32/
+
+echo Zipping...
+
+curdir="$PWD"
+
+cd $B/fmu/
+zip -r ../$name.fmu .
+
+cd $curdir
 }
 
 for D in `find . -maxdepth 1 -type d ! -path . ! -path *.git ! -path */includes ! -path */toolchains`
 do
 		echo "Folder is ${D}"
+name=`echo $D| sed 's|\./||g'`
 
 		compileDarwin $D
 		compileLinux64 $D
 		compileLinux32 $D
+		compileWin64 $D
 		compileWin32 $D
-		compileWin32 $D
-		
+
+		assemble $D $name
 done
 
