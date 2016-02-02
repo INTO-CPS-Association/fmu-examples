dde2f3258490f4cdd6cf2a4419cb171081321fcf
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-02-02 13:24:19 +0100

    added win tool chains + loop for xcompile

--- xcompile.sh
+++ xcompile.sh
@@ -1,7 +1,11 @@
-
 #!/bin/bash
 
-B=threewatertank1/build/Darwin/
+function compileDarwin
+{
+
+echo Darwin
+B=$1/build/Darwin
+echo $B
 
 rm -r $B
 mkdir -p $B
@@ -9,3 +13,14 @@ mkdir -p $B
 cmake  -B$B -Hthreewatertank1/ -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/osx-gcc.cmake`
 
 make -C $B
+
+}
+
+for D in `find . -maxdepth 1 -type d ! -path . ! -path *.git ! -path */includes ! -path */toolchains`
+do
+	echo "Folder is ${D}"
+
+compileDarwin $D
+
+done
+
