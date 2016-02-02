ac40d1ede52173be58edd0fa2079a465f300d848
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-02-02 13:02:42 +0100

    toolchain must be explicit

--- xcompile.sh
+++ xcompile.sh
@@ -1,9 +1,11 @@
+
 #!/bin/bash
 
 B=threewatertank1/build/Darwin/
 
+rm -r $B
 mkdir -p $B
 
-cmake  -B$B -Hthreewatertank1/ -DCMAKE_TOOLCHAIN_FILE=toolchains/osx-gcc.cmake
+cmake  -B$B -Hthreewatertank1/ -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/osx-gcc.cmake`
 
 make -C $B
